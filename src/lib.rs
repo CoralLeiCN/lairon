bc8204a06c0420e10ec4331cee88a7a82591b1fe
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-29 23:21:34 +0000

    Write a Python module in Rust that take numpy array as input

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyArrayMethods, PyReadonlyArrayDyn};
 use pyo3::prelude::*;
+use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -8,7 +11,28 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn lairon(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn lairon<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    // example using immutable borrows producing a new array
+    fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
+        a * &x + &y
+    }
+
+    // wrapper of `axpy`
+    #[pyfn(m)]
+    #[pyo3(name = "axpy")]
+    fn axpy_py<'py>(
+        py: Python<'py>,
+        a: f64,
+        x: PyReadonlyArrayDyn<'py, f64>,
+        y: PyReadonlyArrayDyn<'py, f64>,
+    ) -> Bound<'py, PyArrayDyn<f64>> {
+        let x = x.as_array();
+        let y = y.as_array();
+        let z = axpy(a, x, y);
+        z.into_pyarray_bound(py)
+    }
+
     Ok(())
 }
