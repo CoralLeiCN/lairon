bdd14b30cb140b489caa8a20fd5ffcc84d0013c4
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-11-01 23:07:20 +0000

    dummy xtab

--- src/descriptive/pivot.rs
+++ src/descriptive/pivot.rs
@@ -1,8 +1,41 @@
-use numpy::ndarray::{Array1, ArrayBase, ArrayD, ArrayView1, ArrayViewD, ArrayViewMutD, IxDyn};
-use numpy::{IntoPyArray, PyArrayDyn, PyArrayMethods, PyReadonlyArrayDyn};
-use pyo3::prelude::*;
-use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
+use itertools::Itertools;
+use numpy::ndarray::{Array, ArrayBase, ArrayView1, IxDyn};
 
-pub fn get_array_lengths<T>(a: ArrayView1<T>, b: ArrayView1<T>) -> (usize, usize) {
+// ArrayViewD, ArrayViewMutD, CowRepr, Array1, ArrayD
+// use numpy::{IntoPyArray, PyArrayDyn, PyArrayMethods, PyReadonlyArrayDyn};
+// use pyo3::prelude::*;
+// use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
+
+pub fn get_array_lengths<'a, T>(a: ArrayView1<'a, T>, b: ArrayView1<'a, T>) -> (usize, usize) {
     (a.len(), b.len())
+}
+
+// crosstab
+pub fn crosstab<'a, T>(a: ArrayView1<'a, T>, b: ArrayView1<'a, T>) -> Array<f64, IxDyn> {
+    // ArrayD<T> {
+    let crosstab_size: (usize, usize) = get_array_lengths(a, b);
+    let crosstab_shape = IxDyn(&[crosstab_size.0, crosstab_size.1]);
+
+    let mut xtab = Array::<f64, _>::zeros(crosstab_shape);
+    for (i, a) in a.iter().enumerate() {
+        for (j, b) in b.iter().enumerate() {
+            xtab[[i, j]] += 1.0;
+            println!(
+                "Index i : {}, Index j : {}, Value: {:?}",
+                i,
+                j,
+                xtab[[i, j]]
+            );
+        }
+    }
+
+    xtab
+    // let mut c = Array::zeros(IxDyn(&[crosstab_size.0, crosstab_size.1]));
+    // c
+    // for (i, a) in a.iter().enumerate() {
+    //     for (j, b) in b.iter().enumerate() {
+    //         c[[i, j]] = a * b;
+    //     }
+    // }
+    // c
 }
