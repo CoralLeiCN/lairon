37ba64621b8df002f9e525bcd7cb2e5a8c87bb84
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-12-04 23:52:37 +0000

    init empty crosstab

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,26 @@ fn main() {
         Array::from_vec(vec![0, 2, 1, 2, 3, 4])
             .into_shape_with_order(ndarray::IxDyn(&[6]))
             .unwrap();
-    let map: HashMap<&i32, i32> = array_value_counts(&arr1);
+    let arr2 = Array::from_vec(vec![0, 1, 2, 3, 4, 5])
+        .into_shape_with_order(ndarray::IxDyn(&[6]))
+        .unwrap();
+    let arr1_value_count: HashMap<&i32, i32> = array_value_counts(&arr1);
+    let arr2_value_count: HashMap<&i32, i32> = array_value_counts(&arr2);
+
+    println!("{:?}", arr1_value_count);
+    println!("{:?}", arr2_value_count);
+    let len_map_arr1: usize = arr1_value_count.len();
+    let len_map_arr2: usize = arr2_value_count.len();
+
+    println!(
+        "unique value of array1 {:?}, unique value of array2 {:?}",
+        len_map_arr1, len_map_arr2
+    );
+
+    // init empty crosstab
+    let mut zero_crosstab: ndarray::ArrayBase<ndarray::OwnedRepr<i32>, ndarray::Dim<[usize; 2]>> =
+        Array::<i32, _>::zeros((len_map_arr1, len_map_arr2));
+    println!("{:?}", zero_crosstab);
     // call crosstab
     // let c: (usize, usize) = get_array_lengths(a.view(), b.view());
     // let xtab_result = crosstab(a.view(), b.view());
