cc4ce47f216ccd82f9a5f10fadb8b262c85b7b86
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-12-15 21:57:48 +0000

    refactor main function to utilize crosstab with ndarray views

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // import the ndarray crate
+use ndarray::Array;
 use numpy::ndarray;
 // import the crosstab function from lib.rs
 pub mod descriptive;
@@ -9,13 +10,7 @@ fn main() {
     // call argsort
     let arr1 = ndarray::arr1(&[0, 2, 1, 2, 3, 4, 2, 2]);
     let arr2 = ndarray::arr1(&[0, 1, 2, 3, 5, 5, 2, 2]);
-    // let arr2 = Array::from_vec(vec![0, 1, 2, 3, 4, 5])
-    //     .into_shape_with_order(ndarray::IxDyn(&[6]))
-    //     .unwrap();
-    // let result = crosstab(arr1, arr2);
-    // println!("{:?}", result);
     // call crosstab
-    // let c: (usize, usize) = get_array_lengths(a.view(), b.view());
-    // let xtab_result = crosstab(a.view(), b.view());
-    // println!("{:?}", xtab_result);
+    let xtab_result = crosstab(arr1.view(), arr2.view());
+    println!("{:?}", xtab_result);
 }
