ee715d07067ebc423d485d69e9f9b9605751c2bd
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-12-20 22:36:27 +0000

    clean crosstab codes

--- src/descriptive/pivot.rs
+++ src/descriptive/pivot.rs
@@ -1,41 +1,44 @@
-use crate::utils::array_value_counts;
-use crate::utils::inverse_index;
+use ndarray::Array1;
 use ndarray::Array2;
+use std::collections::HashSet;
+
 use numpy::ndarray;
-use numpy::ndarray::Array;
-use numpy::ndarray::ArrayView;
-use numpy::ndarray::ArrayView1;
 
-// crosstab
-pub fn crosstab<'a>(arr1: ArrayView1<'a, i32>, arr2: ArrayView1<'a, i32>) -> Array2<i32> {
-    // ArrayD<T> {
-    let inv_idx1 = inverse_index(arr1);
-    let inv_idx2 = inverse_index(arr2);
-    // println!("{:?}", inv_idx1);
-    // println!("{:?}", inv_idx2);
+pub fn crosstab(arr1: &Array1<i32>, arr2: &Array1<i32>) -> Array2<usize> {
+    // Get unique values
+    let unique1: Vec<i32> = arr1
+        .iter()
+        .copied()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
+    let unique2: Vec<i32> = arr2
+        .iter()
+        .copied()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
 
-    let arr1_value_count = array_value_counts(arr1);
-    let arr2_value_count = array_value_counts(arr2);
+    // Sort unique values
+    let mut unique1 = unique1;
+    let mut unique2 = unique2;
+    unique1.sort();
+    unique2.sort();
 
-    // println!("{:?}", arr1_value_count);
-    // println!("{:?}", arr2_value_count);
-    let len_map_arr1: usize = arr1_value_count.len();
-    let len_map_arr2: usize = arr2_value_count.len();
+    // Create result matrix
+    let mut result = Array2::<usize>::zeros((unique1.len(), unique2.len()));
 
-    // println!(
-    //     "unique value of array1 {:?}, unique value of array2 {:?}",
-    //     len_map_arr1, len_map_arr2
-    // );
-
-    // init empty crosstab
-    let mut result = Array2::<i32>::zeros((len_map_arr1, len_map_arr2));
-    // println!("{:?}", result);
-
-    for ((_i, a), (_j, b)) in inv_idx1.iter().enumerate().zip(inv_idx2.iter().enumerate()) {
-        result[(*a as usize, *b as usize)] += 1;
-        // println!("Index i : {}, Index j : {}", a, b)
+    // Fill the matrix with frequencies
+    for (i, &val1) in unique1.iter().enumerate() {
+        for (j, &val2) in unique2.iter().enumerate() {
+            let count = arr1
+                .iter()
+                .zip(arr2.iter())
+                .filter(|(&a, &b)| a == val1 && b == val2)
+                .count();
+            result[[i, j]] = count;
+        }
     }
-    // println!("{:?}", result);
 
     result
 }
--- src/descriptive/pivot_bindings.rs
+++ src/descriptive/pivot_bindings.rs
@@ -1,58 +1,13 @@
 use crate::descriptive::pivot::crosstab;
+
 use numpy::{IntoPyArray, PyArray2, PyReadonlyArray1};
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
-use pyo3::{types::PyModule, Bound, PyResult};
+use pyo3::{Bound, PyResult};
 // official example
 //https://github.com/PyO3/pyo3
 // use crate::descriptive::pivot::crosstab;
 /// Formats the sum of two numbers as string.
 // import the ndarray crate
-use ndarray::Array;
-use numpy::ndarray;
-
-use ndarray::{Array1, Array2, Axis};
-use std::collections::HashSet;
-
-pub fn crosstab1(arr1: &Array1<i32>, arr2: &Array1<i32>) -> Array2<usize> {
-    // Get unique values
-    let unique1: Vec<i32> = arr1
-        .iter()
-        .copied()
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect();
-    let unique2: Vec<i32> = arr2
-        .iter()
-        .copied()
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect();
-
-    // Sort unique values
-    let mut unique1 = unique1;
-    let mut unique2 = unique2;
-    unique1.sort();
-    unique2.sort();
-
-    // Create result matrix
-    let mut result = Array2::<usize>::zeros((unique1.len(), unique2.len()));
-
-    // Fill the matrix with frequencies
-    for (i, &val1) in unique1.iter().enumerate() {
-        for (j, &val2) in unique2.iter().enumerate() {
-            let count = arr1
-                .iter()
-                .zip(arr2.iter())
-                .filter(|(&a, &b)| a == val1 && b == val2)
-                .count();
-            result[[i, j]] = count;
-        }
-    }
-
-    result
-}
-
 #[pyfunction]
 pub fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
@@ -67,20 +22,6 @@ pub fn crosstab_bindings<'py>(
     // fn crosstab_bindings(a: PyArrayDyn<i32>, b: PyArrayDyn<i32>) -> PyArrayDyn<i32> {
     let a = a.as_array();
     let b = b.as_array();
-    let result = crosstab1(&a.to_owned(), &b.to_owned());
+    let result = crosstab(&a.to_owned(), &b.to_owned());
     result.into_pyarray(py)
 }
-
-// #[pyfunction]
-// fn crosstab_bindings<'py>(
-//     py: Python<'py>,
-//     a: PyReadonlyArray1<'py, i32>,
-//     b: PyReadonlyArray1<'py, i32>,
-// ) -> Bound<'py, PyArray2<i32>> {
-//     // fn crosstab_bindings(a: PyArrayDyn<i32>, b: PyArrayDyn<i32>) -> PyArrayDyn<i32> {
-//     let a = a.as_array();
-//     let b = b.as_array();
-//     let result: ndarray::ArrayBase<ndarray::OwnedRepr<i32>, ndarray::Dim<[usize; 2]>> =
-//         crosstab(a, b);
-//     result.into_pyarray(py)
-// }
